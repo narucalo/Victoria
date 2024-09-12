c64340166cb0a81dd7b9425a844d31a10525bc0b
Victoria Joseph | magnolianroot <110946840+magnolianroot@users.noreply.github.com>
2024-09-12 02:29:47 -0400

    skills

--- victoriawebsite/src/App.jsx
+++ victoriawebsite/src/App.jsx
@@ -15,6 +15,7 @@ const App = () => {
             element={
               <>
                 <Routes>
+                  <Route path='/home' element={<Home />} />
                   <Route path='/about' element={<About />} />
                   <Route path='/projects' element={<Projects />} />
                   <Route path='/contact' element={<Contact />} />
@@ -31,55 +32,3 @@ const App = () => {
 
 
 
-import './Navbar.css';
-
-const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-left">
-        <Link to="/" className="navbar-logo">
-          <button className="logo-button"><img src="/favicon-32x32.png" alt="Logo" width="30" height="30"/>
-          </button>
-        </Link> 
-      </div>
-      <div className="navbar-center">
-        <ul className="nav-items">
-          <li>
-            <Link to="/home">
-              <button className="nav-button">Home</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/products">
-              <button className="nav-button">About</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/about">
-              <button className="nav-button">Portfolio</button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact">
-              <button className="nav-button">Contact</button>
-            </Link>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-right">
-        <Link to="/resume">
-          <button className="resume-link">
-          <a href="experienced-web-dev-pdf.pdf" target="_blank" rel="noopener noreferrer">my resume</a>
-          </button>
-        </Link>
-      </div>
-    </nav>
-  );
-};
-
-export default Navbar;
-
-
-
-
-export default App;
