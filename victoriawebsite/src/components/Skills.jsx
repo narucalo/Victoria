32706b274d735d3e7424c9e7b4083ea1e7011078
Victoria Joseph <josephv1791@gmail.com>
2024-09-14 20:47:08 -0400

    skills-list

--- victoriawebsite/src/components/Skills.jsx
+++ victoriawebsite/src/components/Skills.jsx
@@ -1,33 +1,69 @@
 // src/components/Skills.jsx
 import React from 'react';
+import JavascriptIcon from '@mui/icons-material/JavascriptSharp';
+import HtmlSharpIcon from '@mui/icons-material/HtmlSharp';
+import CssSharpIcon from '@mui/icons-material/CssSharp';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Box from '@mui/material/Box';
+import { ThemeProvider } from '@mui/material/styles';
 
-const Skills = () => {
+
+
+function SkillArray() {
+  const SkillList =  ["JavaScript", "React", "Bootstrap", "HTML", "CSS", "Node.js", "Figma", "Git" ];
+  
   return (
-    <section id="skills-placeholder" className="placeholder">
-      <div className="container flex-wrap border border-2">
-        <div className="row flex-grow-1">
-          <p className="unselectable">
-            <strong>Skills: 2+ yrs of Front-End Developer Experience with a focus on MERN and Back End.</strong><br />
-            As of 8/24, I'm working on creating:
-            <br /> a RESTful API using ASP.NET Core
-            <br /> a WordPress site with PHP.
-            <br /> a Ren'Py game
-          </p>
-          <ul className="list-group list-group-horizontal list-group-flush">
-            <li className="list-group-item"><i className="fab fa-html5 fa-2x"></i></li>
-            <li className="list-group-item"><i className="fab fa-css3 fa-2x"></i></li>
-            <li className="list-group-item"><i className="fab fa-js fa-2x"></i></li>
-            <li className="list-group-item"><i className="fab fa-mdb fa-2x"></i></li>
-            <li className="list-group-item"><i className="fab fa-bootstrap fa-2x"></i></li>
-            <li className="list-group-item"><i className="fab fa-php fa-2x"></i></li>
-            <li className="list-group-item"><i className="fab fa-laravel fa-2x"></i></li>
-            <li className="list-group-item"><i className="fab fa-node-js fa-2x"></i></li>
-            <li className="list-group-item"><i className="fab bi-filetype-sql fa-2x"></i></li>
-          </ul>
-        </div>
-      </div>
-    </section>
+    <div>
+      <ul>
+        {SkillList.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
   );
-};
+}
 
-export default Skills;
+
+
+export default function BoxSx() {
+  return (
+    <ThemeProvider
+      theme={{
+        palette: {
+          primary: {
+            main: '#007FFF',
+            dark: '#0066CC',
+          },
+        },
+      }}
+    >
+      <Box
+        sx={{
+          width: 600,
+          height: 100,
+          borderRadius: 1,
+          bgcolor: 'primary.main',
+          '&:hover': {
+            bgcolor: 'primary.dark',
+          },
+        }}
+      />
+    </ThemeProvider>
+  );
+}
+
+
+
+
+
+
+
+function Post() {
+  return (
+    <>
+      <PostTitle />
+      <PostBody />
+    </>
+  );
+}
